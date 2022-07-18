cfddd40de28399cc36385e7cf00f2456caca9798
18297920312 <2635627340@qq.com>
2022-07-18 13:13:33 +0800

    deploy

---
+++ precache-manifest.8d6f7206b6e8af6a4644ff1e27df60e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0945c4424ff19aa026d9",
+    "url": "/MoneyNote-website/css/app.26c598a1.css"
+  },
+  {
+    "revision": "e17f111d7261db16ab110850499e0ac2",
+    "url": "/MoneyNote-website/index.html"
+  },
+  {
+    "revision": "0945c4424ff19aa026d9",
+    "url": "/MoneyNote-website/js/app.6520e88c.js"
+  },
+  {
+    "revision": "95eba0626c938d7002c7",
+    "url": "/MoneyNote-website/js/chunk-vendors.0c75790d.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/MoneyNote-website/manifest.json"
+  },
+  {
+    "revision": "45ab67c36769c14eca454f78ed1fca88",
+    "url": "/MoneyNote-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MoneyNote-website/robots.txt"
+  }
+]);
